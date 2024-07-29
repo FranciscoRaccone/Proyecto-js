ba11ffb39fcf7e34678462cebc21c0480a57ae9a
Francisco Raccone <raccone.francisco@gmail.comgit config --global user.email raccone.francisco@gmail.com>
2024-07-28 22:20:58 -0300

    primer-commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+function calcularTotal() {
+    let numeroDeProductos;
+    let total = 0;
+
+    // Validar la cantidad de productos
+    do {
+        numeroDeProductos = parseInt(prompt("Ingresa cantidad de productos:"));
+        if (isNaN(numeroDeProductos) || numeroDeProductos <= 0) {
+            alert("Por favor ingrese un número de productos válido.");
+        }
+    } while (isNaN(numeroDeProductos) || numeroDeProductos <= 0);
+
+    // Validar el precio de cada producto
+    for (let i = 0; i < numeroDeProductos; i++) {
+        let precioProducto;
+
+        do {
+            precioProducto = parseFloat(prompt("Ingresa el precio del producto " + (i + 1) + ":"));
+            if (isNaN(precioProducto) || precioProducto <= 0) {
+                alert("Por favor, ingresa un precio válido.");
+            }
+        } while (isNaN(precioProducto) || precioProducto <= 0);
+
+        total += precioProducto;
+    }
+
+    // Aplicar descuento
+    let descuento = parseFloat(prompt('Ingresa el descuento (%):'));
+    
+    if (isNaN(descuento) || descuento < 0) {
+        alert('Por favor, ingresa un descuento válido.');
+    } else if (descuento > 0) {
+        total = total - (total * (descuento / 100));
+    }
+
+    alert('El costo total es: ' + total.toFixed(2));
+}
+
+// Ejecución del simulador
+calcularTotal();
